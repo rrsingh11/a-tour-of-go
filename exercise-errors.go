69f624349a3c4636dc9f87470e9438adea65f9db
Ritu Raj Singh <rrsingh1101@gmail.com>
2023-08-23 11:42:50 +0530

    Create exercise-errors.go

---
+++ exercise-errors.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
+}
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	var prev float64
+	for {
+		z, prev = z - (z*z - x) / (2*z), z
+		if math.Abs(prev - z) < 1e-6 {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
